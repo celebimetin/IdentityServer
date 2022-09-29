6e89b392f71886996c32eb0eb39a8faa0c77ce40
Metin Çelebi <metincelebi5534@gmail.com>
2022-09-29 18:54:34 +0300

    Bölüm.6 Custom Üyelik Sistemi (Merkezi) tamamlandı

---
+++ IdentityServer.AuthServer/Repository/CustomUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer.AuthServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer.Repository
+{
+    public class CustomUserRepository : ICustomUserRepository
+    {
+        private readonly CustomDbContext _dbContext;
+
+        public CustomUserRepository(CustomDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<CustomUser> FindByEmail(string email)
+        {
+           return await _dbContext.CustomUsers.FirstOrDefaultAsync(x => x.Email == email);
+        }
+        public async Task<CustomUser> FindById(int id)
+        {
+            return await _dbContext.CustomUsers.FindAsync(id);
+        }
+        public async Task<bool> Validate(string email, string password)
+        {
+            return await _dbContext.CustomUsers.AnyAsync(x => x.Email == email & x.Password == password);
+        }
+    }
+}
